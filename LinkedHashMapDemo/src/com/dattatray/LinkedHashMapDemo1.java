d76795c127f13eae21c8e37ee305743f2ab57258
Niket <halmaresniket@gmail.com>
2022-08-09 19:11:49 +0530

    commit 6

--- LinkedHashMapDemo/src/com/dattatray/LinkedHashMapDemo1.java
+++ LinkedHashMapDemo/src/com/dattatray/LinkedHashMapDemo1.java
@@ -7,7 +7,7 @@ public class LinkedHashMapDemo1 {
 	public static void main(String[] args) {
 		LinkedHashMap linkedHashMap = new LinkedHashMap();
 		linkedHashMap.put(15,"yogesh");
-		linkedHashMap.put(12,"samir");
+		linkedHashMap.put(12,"sachin");
 		linkedHashMap.put(20,"sohan");
 
 		System.out.println(linkedHashMap);
